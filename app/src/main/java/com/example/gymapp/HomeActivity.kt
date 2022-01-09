88aa60cf1ee2386886f1833c8db000e44b684ae2
AKAMasterMind404 <59548859+AKAMasterMind404@users.noreply.github.com>
2022-01-09 23:38:23 +0530

    Revert "tried navbar"
    This reverts commit 0c243b8ecd7f862fd1fb6b2271bb4d890fd2e8b6.

--- app/src/main/java/com/example/gymapp/HomeActivity.kt
+++ app/src/main/java/com/example/gymapp/HomeActivity.kt
@@ -6,87 +6,84 @@ import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.FrameLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
-import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gymapp.*
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), TodoClickInterface, TodoClickDeleteInterface,
+    TodoClickDoneInterface {
+    lateinit var todosRV: RecyclerView
     lateinit var addFAB: FloatingActionButton
+    lateinit var viewModel: TodoViewModel
     private lateinit var auth: FirebaseAuth
-
-    lateinit var homeFragment: homeFragment
-    lateinit var profileFragment: profileFragment
-    lateinit var settingsFragment: settingsFragment
 
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        todosRV = findViewById(R.id.idRVTodos)
+        addFAB = findViewById(R.id.idFABAddTodo)
+        todosRV.layoutManager = LinearLayoutManager(this)
+
         auth = FirebaseAuth.getInstance()
 
-        val bottomNav: BottomNavigationView = findViewById(R.id.bottomNavMenu)
-        var frame: FrameLayout = findViewById(R.id.frame_layout)
-
-        homeFragment = homeFragment()
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.frame_layout, homeFragment)
-            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-            .commit()
-
-        bottomNav.setOnNavigationItemReselectedListener { item ->
-            when (item.itemId) {
-                R.id.home->{
-                    homeFragment = homeFragment()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame_layout, homeFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .commit()
-                }
-                R.id.settings->{
-                    settingsFragment = settingsFragment()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame_layout, settingsFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .commit()
-                }
-                R.id.profile->{
-                    profileFragment = profileFragment()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame_layout, profileFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .commit()
-                }
-            }
-        }
-
         val logoutButton: Button = findViewById(R.id.btLogout)
-        logoutButton.setOnClickListener{
+        logoutButton.setOnClickListener(){
             auth.signOut()
             startActivity(Intent(this, MainActivity::class.java))
         }
-        addFAB = findViewById(R.id.idFABAddTodo)
+
+        val todoRVAdapter = TodoRVAdapter(this, this, this, this)
+        todosRV.adapter = todoRVAdapter
+        viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(TodoViewModel::class.java)
+        viewModel.allTodos.observe(this, Observer { list ->
+            list?.let {
+                todoRVAdapter.updateList(it)
+            }
+        })
 
         addFAB.setOnClickListener{
             val intent = Intent(this@HomeActivity, AddEditTodoActivity::class.java)
             startActivity(intent)
         }
 
+
     }
+
+
+    override fun onTodoClick(todo: Todo) {
+        val intent = Intent(this@HomeActivity, AddEditTodoActivity::class.java)
+        intent.putExtra("todoType", "Edit")
+        intent.putExtra("todoTitle", todo.todoTitle)
+        intent.putExtra("todoDescription", todo.todoDescription)
+        intent.putExtra("noteID", todo.id)
+
+        startActivity(intent)
+
+    }
+
+    override fun onDeleteIconClick(todo: Todo) {
+        viewModel.deleteTodo(todo)
+        Toast.makeText(this, "${todo.todoTitle} Deleted", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onTodoDoneClick(todo: Todo) {
+        val updateTodo = Todo(todo.todoTitle, todo.todoDescription, todo.timestamp)
+
+        updateTodo.id = todo.id
+        updateTodo.todoDone = !todo.todoDone
+        viewModel.updateTodo(updateTodo)
+
+    }
+
 
 }
 //{
--- app/src/main/java/com/example/gymapp/MainActivity.kt
+++ app/src/main/java/com/example/gymapp/MainActivity.kt
@@ -4,9 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.FrameLayout
-import androidx.fragment.app.FragmentTransaction
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -19,17 +16,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         auth = FirebaseAuth.getInstance()
+
     }
 
     public override fun onStart() {
         super.onStart()
 
         val currentUser = auth.currentUser
-        if (currentUser == null) {
+        if (currentUser == null){
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        } else {
+        } else{
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
